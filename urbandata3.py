73809a266e7eca5143bf71fc24eb2fea2ecfb270
David Lindberg <dlindberg3@gmail.com>
2015-12-28 20:33:53 -0500

    Removed remaining files

--- urbandata3.py
+++
@@ -1,4 +0,0 @@
-with open('ruraldata/lecz-urban-rural-population-land-area-estimates_codebook.csv','rU') as inputFile:
-	for line in inputFile:
-		line = line.rstrip().split(',')
-		print(len(line))
